623959754ef1e5e1158830db263036a58004f9b2
andrasputyera <andras.putyera@gmail.com>
2021-06-02 17:00:27 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <header>
+    <router-link to="/">
+      <h1><span>Vue</span>deo</h1>
+    </router-link>
+  </header>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Exo', sans-serif;
+
+  &::selection {
+    background: transparentize($color: #01ae72, $amount: 0.5);
+  }
 }
 
-#nav {
-  padding: 30px;
+body {
+  background-color: #e6edf4;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+a {
+  text-decoration: none;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #3f556d;
+  box-shadow: 0px 0px 6px rgba(0,0,0,0.1);
+
+  h1 {
+    color: #cdfcec;
+    font-size: 28px;
+
+    span {
+      color:#01ae72;
     }
   }
 }
