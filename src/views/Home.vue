de876c6cf66e90909b01f9b28de56a6a460642de
andrasputyera <andras.putyera@gmail.com>
2021-06-02 17:51:45 +0200

    added styles to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
-    Home
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img src="../assets/hollywood.jpg" alt="Hollywood Poster" class="featured-img">
+        <div class="detail">
+          <h3>A Century of Film</h3>
+          <p>Hollywood, also called Tinseltown, district within the city of Los Angeles, California, U.S., whose name is synonymous with the American film industry.</p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -13,3 +21,35 @@ export default {
   
 }
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+      position: relative;
+      z-index: 0;
+    }
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+      padding: 16px;
+      z-index: 1;
+      h3 {
+        color:#FFF;
+        margin-bottom: 16px;
+      }
+      p {
+        color: #FFF;
+      }
+    }
+  }
+}
+</style>
