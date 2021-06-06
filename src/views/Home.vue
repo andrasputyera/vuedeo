0b790a40cc3356f6bc6fa910542cb63e82051e97
andrasputyera <andras.putyera@gmail.com>
2021-06-06 23:55:37 +0200

    set up the api fetch for movies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,21 +10,58 @@
       </router-link>
     </div>
 
-    <form @submit.prevent="" class="search-box">
-      <input type="text" placeholder="Movie search..." />
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" placeholder="Movie search..." v-model="search" />
       <input type="submit" value="Search" />
     </form>
 
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
     
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { ref } from 'vue';
+import env from '@/env.js'
 
 export default {
   name: 'Home',
+  setup() {
+    const search = ref("");
+    const movies = ref([]);
+    
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${env.apikey}&s=${search.value}`)
+        .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = "";
+          });  
+      }
+    }
+
+    return {
+      search,
+      movies,
+      SearchMovies
+    }
+  }
+  
   
 }
 </script>
