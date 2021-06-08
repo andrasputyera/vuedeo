1c33bf9ae885adda9133be71b4521309d311e9f0
andrasputyera <andras.putyera@gmail.com>
2021-06-08 23:38:48 +0200

    setup firebase & login/register views

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6-U7VPCj_QSQv4zwbmRtyfRTHZQ9aCSU",
+    authDomain: "vue-3-vuedeo.firebaseapp.com",
+    projectId: "vue-3-vuedeo",
+    storageBucket: "vue-3-vuedeo.appspot.com",
+    messagingSenderId: "746970468108",
+    appId: "1:746970468108:web:5c6e605a389ee5e5120329"
+  };
+
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import MovieDetail from '../views/MovieDetail.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/movie/:id',
     name: 'Movie Detail',
     component: MovieDetail
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   },
 ]
 
